da37f8c5ef7065f97c6ce88261b5f54ab703e232
Quinton Jason <quinton.jason@gmail.com>
2018-01-07 16:33:58 -0600

    updated config and node
    add typography js, filesystem, and transform for markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,22 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
+	siteMetadata: {
+		title: `Q's Fake Site`,
+	},
+  plugins: [
+  `gatsby-transformer-remark`,
+  `gatsby-plugin-glamor`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`,
+    },
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+	{
+		resolve: 'gatsby-plugin-typography',
+		options: {
+			pathToConfigModule: 'src/utils/typography.js'
+		}
+	}
+  ]
+};
