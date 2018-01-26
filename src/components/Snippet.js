5ed43f990575c8b5b01b75c585ef7747f49c7469
Quinton Jason <quinton.jason@gmail.com>
2018-01-26 13:06:05 -0600

    added styled components

--- src/components/Snippet.js
+++ src/components/Snippet.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from "styled-components"
+
+const Li = ({ className, children }) => (
+	<li className={className}>
+		{children}
+	</li>
+)
+
+const SnippetLi = styled(Li)`
+	margin-bottom: 1rem;
+`;
 
 const Snippet = (props) => {
 	const {id, description, username, avatar, time} = props;
 	return(
-		<li className="snippet-wrapper">
+		<SnippetLi className="snippet-wrapper">
 			<a data-id={props.id} href={`/snippets/${props.id}`}>{props.description}</a>
-		</li>
+		</SnippetLi>
 	)
 }
 
--- src/components/SnippetList.js
+++ src/components/SnippetList.js
@@ -1,16 +1,28 @@
 import React from 'react';
-
+import styled from "styled-components"
 
 import snippets from '../../data/gists.json';
 import Snippet from './Snippet';
 
-const SnippetList = props => {
+const Ul = ({ className, children }) => (
+	<ul className={className}>
+		{children}
+	</ul>
+);
+
+const SnippetUl = styled(Ul)`
+	list-style-type: none;
+	padding-left: 0;
+`;
+
+const SnippetList = (props) => {
 
 	const snippetsList = snippets.map(item => <Snippet key={item.index} {...item} />);
 	return(
-		<ul className="snippet-list">
+		
+		<SnippetUl className="snippet-list">
 			{snippetsList}
-		</ul>
+		</SnippetUl>
 	)
 }
 
