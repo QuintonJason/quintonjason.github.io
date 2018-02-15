5b8342c6f09fd042f63ada24c70184a359caf75e
Quinton Jason <quinton.jason@gmail.com>
2018-02-14 19:42:50 -0600

    ig push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,79 @@
-import React from 'react';
+import * as PropTypes from "prop-types"
+import React, {Component} from 'react';
 import Link from 'gatsby-link';
 import { NavLink } from 'react-router-dom';
 
 import Logo from '../images/Logo'
 
-const Header = () => (
-  <header>
-    <div
-      className="header-wrapper"
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    > 
+export default class Header extends Component {
 
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          <Logo className="header-logo"/>
-        </Link>
-      </h1>
-      <nav id="main-nav" className="header-nav">
-        <NavLink to="/posts/" className="header-link">
-          Blog
-        </NavLink>
-        <NavLink to="/snippets/" className="header-link">
-          Snippets
-        </NavLink>
-        <NavLink to="/contact/" className="header-link">
-          Contact
-        </NavLink>
-      </nav>
-    </div>
-  </header>
-)
+	// define props
+	constructor(props) {
+		// propTypes = {}
+	// fires before component is mounted
+		super(props); // makes this refer to this component
+		this.state = {date: new Date()}; // set state
+	}
+	render() {
+		return (
+			<header>
+			  <div
+			    className="header-wrapper"
+			    style={{
+			      margin: '0 auto',
+			      maxWidth: 960,
+			      padding: '1.45rem 1.0875rem',
+			    }}
+			  > 
 
-export default Header
+			    <h1 style={{ margin: 0 }}>
+			      <Link
+			        to="/"
+			        style={{
+			          color: 'white',
+			          textDecoration: 'none',
+			        }}
+			      >
+			        <Logo className="header-logo"/>
+			      </Link>
+			    </h1>
+			    <nav id="main-nav" className="header-nav">
+			      <NavLink to="/posts/" className="header-link">
+			        Blog
+			      </NavLink>
+			      <NavLink to="/snippets/" className="header-link">
+			        Snippets
+			      </NavLink>
+			      <NavLink to="/contact/" className="header-link">
+			        Contact
+			      </NavLink>
+			    </nav>
+			  </div>
+			</header>
+		)
+	}
+
+	componentWillMount() {
+	// fires immediately before the initial render
+	}
+	componentDidMount() {
+	// fires immediately after the initial render
+	}
+	componentWillReceiveProps() {
+	// fires when component is receiving new props
+	}
+	shouldComponentUpdate() {
+	// fires before rendering with new props or state
+	}
+	componentWillUpdate() {
+	// fires immediately before rendering
+	// with new props or state
+	}
+	componentDidUpdate() {
+	// fires immediately after rendering with new P or S
+	}
+	componentWillUnmount() {
+	// fires immediately before component is unmounted
+	// from DOM (removed)
+	}
+}
