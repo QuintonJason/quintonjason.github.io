372de529c2c9713fa23f7f026976637da3226b2c
Quinton Jason <quinton.jason@gmail.com>
2018-03-21 22:58:08 -0500

    fixed sass bug
    needed to install gatsby-plugin-postcss-sass because of blank style.css file based on https://github.com/gatsbyjs/gatsby/issues/3581

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,81 +1,88 @@
-const path = require(`path`)
+const path = require(`path`);
+const lost = require(`lost`);
 
 module.exports = {
-	siteMetadata: {
-		title: `quintonjason.com `,
+  siteMetadata: {
+    title: `quintonjason.com `,
     description: `My Portfolio Site`
-	},
+  },
   plugins: [
-  `gatsby-plugin-sass`,
-  `gatsby-plugin-sharp`,
-  `gatsby-transformer-json`,
-  `gatsby-transformer-sharp`,
-  {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        `gatsby-remark-copy-linked-files`,
-        {
-          resolve: `gatsby-remark-responsive-image`,
-          options: {
-            maxWidth: 590,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-json`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: `gatsby-remark-responsive-image`,
+            options: {
+              maxWidth: 590
+            }
           },
-        },
-        {
-          resolve: 'gatsby-remark-responsive-iframe',
-          options: {},
-        },
-        {
-          resolve: 'gatsby-remark-images',
-          pathPrefix: '/pages',
-          options: {
-            maxWidth: 590,
+          {
+            resolve: `gatsby-plugin-postcss-sass`,
+            options: {
+              postCssPlugins: [lost()]
+            }
           },
-        },
-        {
-          resolve: `gatsby-remark-prismjs`,
-          options: {
-            classPrefix: "language-",
+          {
+            resolve: "gatsby-remark-responsive-iframe",
+            options: {}
+          },
+          {
+            resolve: "gatsby-remark-images",
+            pathPrefix: "/pages",
+            options: {
+              maxWidth: 590
+            }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-"
+            }
           }
-        },
-      ]
-    }
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `data`,
-      path: `${__dirname}/data`,
+        ]
+      }
     },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `pages`,
-      path: `${__dirname}/src/pages`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data`
+      }
     },
-  },
-  {
-    resolve: `gatsby-plugin-google-fonts`,
-    options: {
-      fonts: [
-        `Montserrat`,
-        `Open+Sans\:400,400i` // you can also specify font weights and styles
-      ]
-    }
-  },
-  {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      trackingId: 'UA-57117612-1',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
     },
-  },
-  'gatsby-plugin-offline'
-	// {
-	// 	resolve: 'gatsby-plugin-typography',
-	// 	options: {
-	// 		pathToConfigModule: 'src/utils/typography.js'
-	// 	}
-	// }
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Montserrat`,
+          `Open+Sans\:400,400i` // you can also specify font weights and styles
+        ]
+      }
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-57117612-1"
+      }
+    },
+    "gatsby-plugin-offline"
+    // {
+    // 	resolve: 'gatsby-plugin-typography',
+    // 	options: {
+    // 		pathToConfigModule: 'src/utils/typography.js'
+    // 	}
+    // }
   ]
 };
