cf5f43bd66da5ea4cd4f625786a62360d58aafb8
Quinton Jason <quinton.jason@gmail.com>
2026-05-30 22:50:58 -0500

    Add view transitions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,36 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+const VIEW_TRANSITION_TIMEOUT = 450
+
+const prefersReducedMotion = () =>
+  window.matchMedia &&
+  window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+exports.onInitialClientRender = () => {
+  if (
+    typeof document === 'undefined' ||
+    typeof document.startViewTransition !== 'function' ||
+    prefersReducedMotion() ||
+    typeof window.___navigateTo !== 'function'
+  ) {
+    return
+  }
+
+  const navigateTo = window.___navigateTo
+
+  window.___navigateTo = pathname => {
+    if (!pathname || pathname === window.location.pathname) {
+      navigateTo(pathname)
+      return
+    }
+
+    document.startViewTransition(
+      () =>
+        new Promise(resolve => {
+          navigateTo(pathname)
+          window.setTimeout(resolve, VIEW_TRANSITION_TIMEOUT)
+        })
+    )
+  }
+}
